056c08cfd20109717c7de0a5150d936cb9c69510
CriaScript <131717638+criascript@users.noreply.github.com>
2025-02-24 11:15:09 -0300

    chore: updating domain config

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -8,10 +8,10 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://criascripters.github.io/',
+  url: 'https://faq.criascript.dev/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/faq/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
